a8081c9d9a3d2c490d88fbf15d82914aef6164d2
rdgh0st <83750015+rdgh0st@users.noreply.github.com>
2023-09-25 16:28:46 -0700

    generating JONSWAP spectrum texture!

---
+++ FFTWater_Test.gd
@@ -0,0 +1,96 @@
+extends MeshInstance3D
+@export_file("*.glsl") var compute_shader
+@export var fetch : float;
+@export var windSpeed : float;
+@export var enhancementFactor: float;
+@export var inputfreq: float;
+@export var resolution: float;
+@export var oceanSize: float;
+@export var transformHorizontal: float;
+@export var lowCutoff: float;
+@export var highCutoff: float;
+@export var depth: float;
+
+var rd: RenderingDevice;
+var shader_rid: RID;
+var uniform_set: RID;
+var pipeline: RID;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	init_gpu();
+
+func init_gpu():
+	rd = RenderingServer.create_local_rendering_device();
+	
+	var shader_file_data: RDShaderFile = load(compute_shader);
+	var shader_spirv: RDShaderSPIRV = shader_file_data.get_spirv();
+	shader_rid = rd.shader_create_from_spirv(shader_spirv);
+	
+	var input = [fetch, windSpeed, enhancementFactor, inputfreq, resolution, oceanSize, 0, transformHorizontal, lowCutoff, highCutoff, depth];
+	
+	var params := PackedFloat32Array(input).to_byte_array();
+	var buffer = rd.storage_buffer_create(params.size(), params);
+	var uniform := RDUniform.new();
+	uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER;
+	uniform.binding = 0;
+	uniform.add_id(buffer);
+	
+	var image = Image.create(resolution, resolution, false, Image.FORMAT_RF);
+	
+	var imageFormat = RDTextureFormat.new();
+	imageFormat.width = resolution;
+	imageFormat.height = resolution;
+	imageFormat.format = RenderingDevice.DATA_FORMAT_R32_SFLOAT;
+	imageFormat.usage_bits = RenderingDevice.TEXTURE_USAGE_CAN_UPDATE_BIT | RenderingDevice.TEXTURE_USAGE_STORAGE_BIT | RenderingDevice.TEXTURE_USAGE_CAN_COPY_FROM_BIT;
+	
+	var image_rid = rd.texture_create(imageFormat, RDTextureView.new(), image.get_data());
+	var imageUniform = RDUniform.new();
+	imageUniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE;
+	imageUniform.binding = 10;
+	imageUniform.add_id(image_rid);
+	
+	uniform_set = rd.uniform_set_create([uniform, imageUniform], shader_rid, 0);
+	
+	pipeline = rd.compute_pipeline_create(shader_rid);
+	var compute_list := rd.compute_list_begin();
+	rd.compute_list_bind_compute_pipeline(compute_list, pipeline);
+	rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0);
+	rd.compute_list_dispatch(compute_list, resolution / 8, resolution / 8, 1);
+	rd.compute_list_end();
+	rd.submit();
+	rd.sync();
+	
+	var output_bytes := rd.buffer_get_data(buffer);
+	var output := output_bytes.to_float32_array();
+	print("Input: ", input)
+	print("Output: ", output)
+	
+	var image_output_bytes := rd.texture_get_data(image_rid, 0);
+	var image_new := Image.create_from_data(resolution, resolution, false, Image.FORMAT_RF, image_output_bytes);
+	var tex := ImageTexture.create_from_image(image_new);
+	$TextureRect.texture = tex;
+
+func _notification(what):
+	if what == NOTIFICATION_PREDELETE:
+		cleanup_gpu();
+
+func cleanup_gpu():
+	if rd == null:
+		return;
+	
+	rd.free_rid(pipeline);
+	pipeline = RID();
+	
+	rd.free_rid(uniform_set);
+	uniform_set = RID();
+	
+	rd.free_rid(shader_rid);
+	shader_rid = RID();
+	
+	rd.free();
+	rd = null;
+
+func _process(delta):
+	if (Input.is_action_just_pressed("move_backward")):
+		init_gpu();
---
+++ Shaders/JONSWAP_test.glsl
@@ -0,0 +1,93 @@
+#[compute]
+#version 460 core
+
+layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
+
+const float PI = 3.14159265359;
+const float g = 9.81;
+
+layout(set = 0, binding = 10, r32f) writeonly uniform image2D spectrum_image;
+
+layout(set = 0, binding = 0) buffer SpectrumParameters {
+    float fetch;
+    float windSpeed;
+    float enhancementFactor;
+    float inputFreq;
+    float resolution;
+    float oceanSize;
+    float time;
+    float transformHorizontal;
+    float lowCutoff;
+    float highCutoff;
+    float depth;
+} params;
+
+float square(float x) {
+    return x * x;
+}
+
+//note: uniformly distributed, normalized rand, [0;1[
+float nrand( vec2 n )
+{
+	return fract(sin(dot(n.xy, vec2(12.9898, 78.233)))* 43758.5453);
+}
+
+const float ALPHA = 0.14;
+const float INV_ALPHA = 1.0 / ALPHA;
+const float K = 2.0 / (PI * ALPHA);
+
+float inv_error_function(float x)
+{
+	float y = log(1.0 - x*x);
+	float z = K + 0.5 * y;
+	return sqrt(sqrt(z*z - y * INV_ALPHA) - z) * sign(x);
+}
+
+float gaussian_rand( vec2 n )
+{
+	float t = fract( params.time );
+	float x = nrand( n + 0.07*t );
+    
+	return inv_error_function(x*2.0-1.0)*0.15 + 0.5;
+}
+
+float JONSWAP(float freq) {
+    float peak = 22.0f * pow(((g * g) / (params.windSpeed * params.fetch)), (1.0f / 3.0f));
+
+    float sigma = (freq <= peak) ? 0.07f : 0.09f;
+
+    float alpha = 0.076 * (pow((params.windSpeed * params.windSpeed) / (params.fetch * g), 0.22f));
+
+    float r = exp(-square(freq - peak) / (2.0f * square(sigma) * square(peak)));
+
+    return ((alpha * square(g)) / pow(freq, 5.0f)) * exp(-1.25 * pow((peak / freq), 4.0f)) * pow(params.enhancementFactor, r);
+}
+
+void main() {
+    ivec2 pixel_coord = ivec2(gl_GlobalInvocationID.xy);
+    /*
+    float n = (pixel_coord.x < 0.5f * params.resolution) ? pixel_coord.x : pixel_coord.x - params.resolution;
+    float m = (pixel_coord.y < 0.5f * params.resolution) ? pixel_coord.y : pixel_coord.y - params.resolution;
+
+    vec2 wave_vector = (2.f * PI * vec2(n, m)) / params.oceanSize;
+    float k = length(wave_vector);
+    */
+    float halfN = params.resolution / 2.0f;
+    float deltaK = 2.0f * PI / params.oceanSize;
+    vec2 k = (pixel_coord.xy - halfN) * deltaK;
+    float kLength = length(k) + params.transformHorizontal;
+
+    if (params.lowCutoff <= kLength && kLength <= params.highCutoff) {
+        float coeff = 1.0f / sqrt(2);
+        float dispersionK = sqrt(g * kLength * tanh(min(kLength * params.depth, 20)));
+        float JSroot = sqrt(JONSWAP(dispersionK));
+        float res = coeff * gaussian_rand(k) * gaussian_rand(pixel_coord) * JSroot;
+
+        imageStore(spectrum_image, pixel_coord, vec4(res, 0.0, 0.0, 0.0));
+    } else {
+        imageStore(spectrum_image, pixel_coord, vec4(0.0));
+    }
+
+    //params.outputValue = JONSWAP(params.inputFreq);
+    //params.outputValue = 22.0f * pow(((g * g) / (params.windSpeed * params.fetch)), (0.33f));
+}
